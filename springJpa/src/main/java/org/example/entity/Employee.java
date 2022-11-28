55497388f256f24b965788b0359db73dbc2d85c1
Jayanttewari <117714540+Jayanttewari@users.noreply.github.com>
2022-11-28 18:54:14 +0530

    spring jpa project

---
+++ springJpa/src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,30 @@
+package org.example.entity;
+
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    private int id;
+    private String lastname;
+    private int salary;
+    private String address;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
